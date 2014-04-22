74f7910d5be97c4633ca709da083e475e958d504
Ethan Taubman <etaubman@gmail.com>
2014-04-21 23:48:26 -0400

    added map folder

--- FirstTry/script.js
+++ FirstTry/script.js
@@ -1,17 +1,18 @@
-var data = [
-	{
-		"x_axis" : 60,
-		"y_axis" : 80,
-		"radius" : 50,
-		"color" : "rgb(0,189,242)"
-	},
-	{
-		"x_axis" : 100,
-		"y_axis" : 200,
-		"radius" : 40,
-		"color" : "rgb(0,14,114)"
+function Circle(){
+	this.x_axis = Math.random() * 100;
+	this.y_axis = Math.random() * 100;
+	this.radius = Math.random() * this.x_axis;
+	this.get_color = function(){
+		hex =  Math.floor(Math.random() * 16777215);
+		return hex.toString(16);
 	}
-]
+	this.color = "#" + this.get_color();
+}
+
+var data = [];
+for (var i = 0; i < 2000; i++) {
+	data.push(new Circle);
+};
 
 var container = d3.select("body").append("svg")
 					.attr("width",window.innerWidth)
@@ -31,10 +32,10 @@ var circleAttributes = circles
 circles
 	.transition()
 	.attr("cx",function(d){
-		return window.innerWidth - d.radius;
+		return Math.random() * (window.innerWidth - d.radius);
 	})
 	.attr("cy",function(d){
-		return window.innerHeight - d.radius;
+		return Math.random() * (window.innerHeight - d.radius);
 	})
 	.duration(4000)
 	.ease("bounce");
