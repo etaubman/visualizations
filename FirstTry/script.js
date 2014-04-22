d09cde5d2d6eb41d78c81371852a8380035a1690
Ethan Taubman <etaubman@gmail.com>
2014-04-21 23:06:13 -0400

    added animation

--- FirstTry/script.js
+++ FirstTry/script.js
@@ -1,7 +1,40 @@
-var myData = [1,2,3,4];
+var data = [
+	{
+		"x_axis" : 60,
+		"y_axis" : 80,
+		"radius" : 50,
+		"color" : "rgb(0,189,242)"
+	},
+	{
+		"x_axis" : 100,
+		"y_axis" : 200,
+		"radius" : 40,
+		"color" : "rgb(0,14,114)"
+	}
+]
 
-var p = d3.select("body").selectAll("p")
-	.data(myData)
-	.enter()
-	.append("p")
-	.text(function(d){return "Whaddup! " + d;});
+var container = d3.select("body").append("svg")
+					.attr("width",window.innerWidth)
+					.attr("height",window.innerHeight);
+
+var circles = container.selectAll("circle")
+					.data(data)
+					.enter()
+					.append("circle");
+
+var circleAttributes = circles
+					.attr("cx",function(d){return d.x_axis})
+					.attr("cy",function(d){return d.y_axis})
+					.attr("r",function(d){return d.radius})
+					.style("fill",function(d){return d.color});
+
+circles
+	.transition()
+	.attr("cx",function(d){
+		return window.innerWidth - d.radius;
+	})
+	.attr("cy",function(d){
+		return window.innerHeight - d.radius;
+	})
+	.duration(4000)
+	.ease("bounce");
